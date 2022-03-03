bebe8914b66f07b60efb2c9be15f77ba6ac30b4e
Eric Ferreria <ferric@google.com>
2022-03-03 02:24:46 +0000

    edit js again

--- visualizations/column.js
+++ visualizations/column.js
@@ -1,5 +1,5 @@
 /**
- * Welcome to the Looker Visualization Builder! Please refer to the following resources 
+ * Welcome to the Looker Visualization Builder! Please refer to the following resources
  * to help you write your visualization:
  *  - API Documentation - https://github.com/looker/custom_visualizations_v2/blob/master/docs/api_reference.md
  *  - Example Visualizations - https://github.com/looker/custom_visualizations_v2/tree/master/src/examples
@@ -22,7 +22,7 @@
          default: 42
        }
      },
-    
+
     /**
      * The create function gets called when the visualization is mounted but before any
      * data is passed to it.
@@ -30,7 +30,7 @@
        create: function(element, config){
            element.innerHTML = "<h1>Ready to render!</h1>";
        },
-   
+
     /**
      * UpdateAsync is the function that gets called (potentially) multiple times. It receives
      * the data and should update the visualization with the new data.
@@ -40,14 +40,14 @@
        var margin = {top: 20, right: 20, bottom: 30, left: 40},
            width = 960 - margin.left - margin.right,
            height = 500 - margin.top - margin.bottom;
-   
+
        // set the ranges
        var x = d3.scaleBand()
                  .range([0, width])
                  .padding(0.1);
        var y = d3.scaleLinear()
                  .range([height, 0]);
-   
+
        // append the svg object to the body of the page
        // append a 'group' element to 'svg'
        // moves the 'group' element to the top left margin
@@ -56,11 +56,11 @@
            .attr("width", width + margin.left + margin.right)
            .attr("height", height + margin.top + margin.bottom)
          .append("g")
-           .attr("transform", 
+           .attr("transform",
                  "translate(" + margin.left + "," + margin.top + ")");
-       
+
        formattedData = []
-   
+
        // format the data
        data.forEach(function(d) {
          formattedData.push({
@@ -69,11 +69,11 @@
            opponent_class: d["game.opponent_class"]["value"]
          });
        });
-   
+
        // Scale the range of the data in the domains
        x.domain(formattedData.map(function(d) { return d.friendly_class; }));
        y.domain([0, d3.max(formattedData, function(d) { return d.count; })]);
-   
+
        // append the rectangles for the bar chart
        svg.selectAll(".bar")
          .data(formattedData)
@@ -84,18 +84,16 @@
          .attr("width", x.bandwidth())
          .attr("y", function(d) { return y(d.count); })
          .attr("height", function(d) { return height - y(d.count); });
-   
+
        // add the x Axis
        svg.append("g")
          .attr("transform", "translate(0," + height + ")")
          .call(d3.axisBottom(x));
-   
+
        // add the y Axis
        svg.append("g")
          .call(d3.axisLeft(y));
-   
+
            doneRendering()
        }
    };
-   
-   looker.plugins.visualizations.add(visObject);
